10131a3761c694adc74e3de1c63dc1011b908905
Sagi Granot <sagigranot@gmail.com>
2019-03-16 21:08:06 +0200

    mergin time with parantheses

--- server.js
+++ server.js
@@ -6,53 +6,12 @@ const cors = require('cors');
 const TEMP_DIR = './tmp'
 const SRC_DIR = './src'
 
+//Create and init new Index object
 var index = new indexFile
 index.extractWords()
 index.buildIndex()
 
-const app  = express()
-const port = process.env.PORT || 8080
-
-
-
-app.use(express.static('tmp'))
-
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-    cb(null, './tmp/')
-    },
-    filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname)
-    }
-    });
-const upload = multer({ storage })
-     
-app.use(cors());
-app.post('/upload', upload.single('image'), (req, res) => {
-    if (req.file){
-        index.updateIndex()
-        res.json({
-        imageUrl: `uploads/${req.file.filename}`
-        });
-    }
-    else 
-    res.status("409").json("No Files to Upload.");
-});
-
-
-app.set('port',port);
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(
-    (req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.set("Content-Type", "application/json");
-        next();
- });
-
-app.get('/search/:query',(req,res) => {
-    const { query = null } = req.params
+function search(query){
     const stopList = ["the", "a", "is", "to", "in", "at"]
     let temp = query.toString().toLowerCase()
     let seder = temp.match(/[a-zA-Z()+-/"]+/g)
@@ -192,8 +151,103 @@ app.get('/search/:query',(req,res) => {
     //console.log(results)
     opAnd = []
     opNot = []
-    res.json(results)
+    
+    return results
+}
+function queryPriority(expression, queryArr = []) {
+    let leftArr = [];
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] === '(') {
+            leftArr.push(i);
+        }
+ 
+        let leftArrLength = leftArr.length;
+        //fail fast when we see close brack before open bracket
+        if (leftArrLength == 0 && expression[i] == ')') {
+            return null;
+        }
+ 
+        //add a query that is wrap with brackets
+        if (leftArrLength > 0 && expression[i] === ')') {
+            const start = leftArr.pop();
+            query = expression.slice(start + 1, i)
+            queryArr.push(expression.slice(start + 1, i).trim());
+            expression = expression.replace(`(${query})`, "");
+            queryArr = queryPriority(expression, queryArr);
+            return queryArr;
+            //add a query that isn't wrap inside brackets
+        } else if (leftArrLength == 0 && i + 1 == expression.length) {
+            queryArr.push(expression.trim());
+            //return null if there is open bracket but no closing bracket
+        } else if (leftArrLength > 0 && i + 1 == expression.length) {
+            console.log('invalid query string');
+            return null;
+        }
+    }
+    return queryArr;
+}
+const app  = express()
+const port = process.env.PORT || 8080
 
+app.use(express.static('tmp'))
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+    cb(null, './tmp/')
+    },
+    filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname)
+    }
+    });
+const upload = multer({ storage })
+     
+app.use(cors());
+app.post('/upload', upload.single('image'), (req, res) => {
+    if (req.file){
+        index.updateIndex()
+        res.json({
+        imageUrl: `uploads/${req.file.filename}`
+        });
+    }
+    else 
+    res.status("409").json("No Files to Upload.");
+});
+
+
+app.set('port',port);
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(
+    (req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.set("Content-Type", "application/json");
+        next();
+ });
+
+
+app.get('/search/:query',(req,res) => {
+    var { query = null } = req.params
+    let results = []
+
+    query = queryPriority(query)
+    query.forEach(term => {
+        console.log(term)
+        results.push(search(term))
+    })
+
+
+    // for(let i=0; i<results.length; ++i) {
+    //     for(let j=i+1; j<results.length; ++j) {
+    //         if(results[i].id === results[j].id){
+    //             results[i].weight += results[j].weight
+    //             results.splice(j--, 1)
+    //         }
+    //     }
+    // }
+
+
+    res.json(results)
 })
 
 app.get('/view/:id/:query',(req,res) => {
